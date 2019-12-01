b56e7aa9dd132d2cd99b2b18f688dc9f2c294348
gbabineau <guy.babineau@gmail.com>
2019-12-01 13:37:03 -0500

    environment

--- tests/test_findAtlasReviewChanges.py
+++ tests/test_findAtlasReviewChanges.py
@@ -11,6 +11,7 @@ def test_version_is_string():
 
 def test_CLI_version(capsys):
     ebird_api_key = os.getenv('EBIRDAPIKEY')
+    assert(len(ebird_api_key)==12)
     review_checklist(ebird_api_key, 'S35636314')
     captured = capsys.readouterr()
     assert ("cangoo" in captured.out)
