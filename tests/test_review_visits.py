3051d282ac07de6117e7e1c5e2acbe76752f00f7
gbabineau <guy.babineau@gmail.com>
2020-06-29 14:24:30 -0400

    fix test

--- tests/test_review_visits.py
+++ tests/test_review_visits.py
@@ -12,6 +12,6 @@ def test_review_visits(capsys):
     assert(len(ebird_api_key) == 12)
     day = parser.parse('2018-04-08')
     end_day = day + datetime.timedelta(days=1)
-    review_visits(ebird_api_key, 'Guy Babineau', 'US-VA-003', day, end_day, 30, True)
+    review_visits(ebird_api_key, 'Guy Babineau', 'US-VA-003', day, end_day, 40, True)
     captured = capsys.readouterr()
     assert ("Chris Greene" in captured.out)
